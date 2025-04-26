be42d0b68d18c5bfd3d20583dfab250bc1277f08
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-04-26 13:36:02 +0100

    Add secret key to settings

--- baker/settings.py
+++ baker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--bq$$aka9h-_h!82t$c3_rc1_5&^bsjrjm=a98krib43sus+0j'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
