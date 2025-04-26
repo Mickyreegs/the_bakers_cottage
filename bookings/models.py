e521061d183db40101f1001fed223cddbd7e7ce9
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-04-26 15:18:09 +0100

    Add class Booking and create models and f string function

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Booking(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_guests = models.IntegerField()
+    special_requests = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Booking for {self.customer.username} on {self.date} at {self.time}"
