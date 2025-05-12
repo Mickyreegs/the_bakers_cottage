ffdb1001a3c89bba8a0b9657532d13207ccf7f8d
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-05-12 20:46:49 +0100

    Add shop main content with images and use for loops to reduce code in django

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import SelectionBox, Cake
 from django.http import HttpResponse
 
 # Create your views here.
 
 def shop(request):
-    return render(request, "shop/shop.html")
+    selection_boxes = SelectionBox.objects.all()
+
+    cakes = Cake.objects.all()
+
+    return render(request, "shop/shop.html", {"selection_boxes": selection_boxes, "cakes": cakes})
+
+
+
+
