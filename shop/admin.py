843ecaf61d1c0cf0a0aa57d44571fb534a33a809
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-05-12 20:46:49 +0100

    Add shop main content with images and use for loops to reduce code in django

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Cake, SelectionBox
 
 # Register your models here.
+
+@admin.register(Cake)
+class CakeAdmin(admin.ModelAdmin):
+    list_display = ("cake_type", "price")
+    search_fields = ("cake_type",)
+
+
+@admin.register(SelectionBox)
+class SelectionBoxAdmin(admin.ModelAdmin):
+    list_display = ("box_type", "price")
+    search_fields = ("box_type",)
+
--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+#Selection boxes
+class SelectionBox(models.Model):
+    box_type = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.box_type} - €{self.price}"
+
+
+
+#Individual cakes
 class Cake(models.Model):
     cake_type = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=6, decimal_places=2)
