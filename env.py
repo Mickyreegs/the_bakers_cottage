4a96b90ea3ff5e54b197de8383c598431eddb821
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-05-28 20:32:25 +0100

    Change implementation to use environment variables to load config values

--- env.py
+++ env.py
@@ -1,19 +1,25 @@
+# importing os module for environment variables
 import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv
+# loading variables from .env file
+load_dotenv() 
 
 os.environ.setdefault(
-    "DATABASE_URL", "")
+    "DATABASE_URL", os.getenv("DATABASE_URL"))
 
 os.environ.setdefault(
-    "SECRET_KEY", "")
-
-os.environ.setdefault(
-    "MAPS_KEY", ""
+    "SECRET_KEY", os.getenv("SECRET_KEY")
 )
 
 os.environ.setdefault(
-    "MAP_ID", ""
+    "MAPS_KEY", os.getenv("MAPS_KEY")
 )
 
 os.environ.setdefault(
-    "EMAILJS_PUBLIC_KEY", ""
+    "MAP_ID", os.getenv("MAP_ID")
+)
+
+os.environ.setdefault(
+    "EMAILJS_PUBLIC_KEY", os.getenv("EMAILJS_PUBLIC_KEY")
 )
