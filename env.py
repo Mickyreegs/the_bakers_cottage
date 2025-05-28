6cf64ed11cb7219b0fc94ee063be2e9a963dbcda
Mickyreegs <w58fyus5@students.codeinstitute.net>
2025-05-28 20:14:17 +0100

    Update env file and amend gitignore

--- env.py
+++ env.py
@@ -1,19 +1,19 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_aR37BtuykWel@ep-shy-wave-a270dbtp.eu-central-1.aws.neon.tech/drive_motor_pecan_964146")
+    "DATABASE_URL", "")
 
 os.environ.setdefault(
-    "SECRET_KEY", "BakeyBakerson123!!!$")
+    "SECRET_KEY", "")
 
 os.environ.setdefault(
-    "MAPS_KEY", "AIzaSyD0DRWxmuEk-dYAoQCmztGE404YTlzLApg"
+    "MAPS_KEY", ""
 )
 
 os.environ.setdefault(
-    "MAP_ID", "3cf0ec6e2eaec88cdcbd8c7c"
+    "MAP_ID", ""
 )
 
 os.environ.setdefault(
-    "EMAILJS_PUBLIC_KEY", "GthYjHLX2fqAsa2FV"
+    "EMAILJS_PUBLIC_KEY", ""
 )
